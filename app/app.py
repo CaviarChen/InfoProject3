82c39421ab27edffa9ae82272d5d4dfbd45dfcc2
CaviarChen <yzj@vlabpro.com>
2016-05-13 16:45:08 +1000

    Add some pages

--- app/app.py
+++ app/app.py
@@ -10,9 +10,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/pivot')
+def pivot():
+    return render_template('pivot.html')
+
 @app.route('/observation')
 def observation():
     return render_template('observation.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.debug = config['server']['debug']
