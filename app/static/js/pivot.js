4aa86a4f7023cf06e12974dae1c5366f2d72b6bf
CaviarChen <yzj@vlabpro.com>
2016-05-19 16:08:06 +1000

    Some Update

--- app/static/js/pivot.js
+++ app/static/js/pivot.js
@@ -1,6 +1,17 @@
+$('#s-filter-1').on('change', function() {
+  bool = ($('#s-filter-1 :selected').val()=="0");
+  $('#s-filter-2').attr('disabled', bool);
+  $('#i-filter-3').attr('disabled', bool);
+});
+
 $('#btn-generate').on('click', function(event) {
-  $.get( "api/PivotTable", { value: $("#select_value :selected").val() } )
+
+  waitingDialog.show('Loading');
+
+  $.get( 'api/PivotTable', { value: $('#s-value :selected').val() } )
   .done(function( data ) {
-    console.log( "Data Loaded: " + data );
+
+    $('#table_c').text(data);
+    waitingDialog.hide();
   });
 });
